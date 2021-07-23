582203228c28e0abe6357e18a7b78de46b49990d
SergMach <87276155+SergMach@users.noreply.github.com>
2021-07-24 00:35:09 +0300

    Модуль расписания

---
+++ session_service/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_session import Session
+
+
+def create_app(config="session_service.config.Config"):
+    app = Flask(__name__)
+
+    with app.app_context():
+        app.config.from_object(config)
+
+        from session_service.models import db, User, Table
+        sess = Session()
+
+        db.init_app(app)
+        sess.init_app(app)
+
+        from session_service.utils.initialization import init_template_globals
+
+        init_template_globals(app)
+
+        if app.config.get("DEBUG"):
+            User.create_mock_users()
+            Table.create_mock_tables()
+
+        from session_service.auth import auth
+
+        app.register_blueprint(auth)
+
+        return app
